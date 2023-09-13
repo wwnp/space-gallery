9806f7dac87e84446aa50d8eabf79637764da114
wwnp <iamtodaysad@gmail.com>
2023-09-13 03:56:24 +0300

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css';
-import Particles from "vue3-particles";
-import masonry from 'vue-next-masonry';
-
-createApp(App).use(router).use(masonry).use(Particles).mount('#app');
-
